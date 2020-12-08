561231925fbfec9894d44aa174931f5152f92d7c
David <davidgalamyan@gmail.com>
2020-12-08 22:46:10 +0400

    Добавьте файлы проекта.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет,  {name}, сегодня [{DateTime.Now}]");
+            
+        }
+    }
+}
